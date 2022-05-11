71e473dac21b93d5ca61c3e66a041adc0d4ebede
EngChooN <aimzero9303@gmail.com>
2022-05-11 20:07:16 +0900

    20220511

---
+++ class/pages/radial-chart/index.tsx
@@ -0,0 +1,12 @@
+import { RadialChart } from "react-vis";
+
+export default function test() {
+  const myData = [{ angle: 1 }, { angle: 5 }, { angle: 6 }, { angle: 6 }];
+
+  return (
+    <>
+      {/* 원형 그래프 */}
+      <RadialChart data={myData} width={300} height={300} />
+    </>
+  );
+}
