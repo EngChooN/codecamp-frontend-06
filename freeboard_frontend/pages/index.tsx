2102a01d1475726581b99686669169337ef9f16c
준영 <aimzero9303@gmail.com>
2022-07-11 06:41:37 +0900

    rendingPage

--- freeboard_frontend/pages/index.tsx
+++ freeboard_frontend/pages/index.tsx
@@ -1,72 +1,39 @@
-import styled from "@emotion/styled";
-import axios from "axios";
-import { useEffect, useState } from "react";
-import Zoom from "react-reveal/Zoom";
+import { useRouter } from "next/router";
 import {
   Box1,
-  Box1_1,
+  Box1_Btns,
+  Box1_Btn_Github,
+  Box1_Btn_Velog,
   Box1_Content,
   Box1_Title,
   Box1_Zoom,
   Box1_Zoom_Wrapper,
+  Box2,
+  Box2_Content,
+  Box2_Content_Wrapper,
+  Box2_Slide,
+  Box3,
+  Box3_Content,
+  Box3_Content_Wrapper,
+  Box3_Slide,
+  Box4,
+  Box4_Content,
+  Box4_Content_Wrapper,
+  Box4_Slide,
+  Box5,
+  Box5_Content,
+  Box5_Content_Wrapper,
+  Box5_Review,
+  Box5_Review_Wrapper,
+  HelloLottie,
+  ReactLottie,
+  StudyLottie,
+  ThinkLottie,
   Wrapper,
 } from "../src/components/units/Home.styles";
-
-// const Wrapper = styled.div`
-//   width: 1200px;
-//   display: flex;
-//   flex-direction: column;
-//   margin: auto;
-//   margin-top: 500px;
-// `;
-
-// const Photo1 = styled.div`
-//   display: flex;
-//   width: 100%;
-// `;
-
-// const Photo1Img = styled.img`
-//   height: 300px;
-//   border-radius: 15px;
-//   width: 100%;
-//   object-fit: cover;
-//   margin: 10px;
-// `;
-
-// const Photo2 = styled.div`
-//   display: flex;
-//   width: 100%;
-// `;
-
-// const Photo2Img = styled.img`
-//   height: 300px;
-//   border-radius: 15px;
-//   width: 100%;
-//   object-fit: cover;
-//   margin: 10px;
-// `;
-
-// const Photo3 = styled.div`
-//   display: flex;
-//   width: 100%;
-// `;
-
-// const Photo3Img = styled.img`
-//   height: 300px;
-//   border-radius: 15px;
-//   width: 100%;
-//   object-fit: cover;
-//   margin: 10px;
-// `;
+import { Tooltip } from "antd";
 
 export default function Home() {
-  // const [loadImg, setLoadImg] = useState("");
-  // const [loadImg2, setLoadImg2] = useState("");
-  // const [loadImg3, setLoadImg3] = useState("");
-  // const [loadImg4, setLoadImg4] = useState("");
-  // const [loadImg5, setLoadImg5] = useState("");
-  // const [loadImg6, setLoadImg6] = useState("");
-
   // useEffect(() => {
   //   const imgfunction = async () => {
   //     const result = await axios.get("https://dog.ceo/api/breeds/image/random");
@@ -75,77 +42,105 @@ export default function Home() {
   //   };
   //   imgfunction();
   // }, []);
+  const router = useRouter();
 
-  // useEffect(() => {
-  //   const imgfunction = async () => {
-  //     const result = await axios.get("https://dog.ceo/api/breeds/image/random");
-  //     setLoadImg2(result.data.message);
-  //     console.log(result);
-  //   };
-  //   imgfunction();
-  // }, []);
+  const onClickGithub = () => {
+    window.open("https://github.com/EngChooN");
+  };
 
-  // useEffect(() => {
-  //   const imgfunction = async () => {
-  //     const result = await axios.get("https://dog.ceo/api/breeds/image/random");
-  //     setLoadImg3(result.data.message);
-  //     console.log(result);
-  //   };
-  //   imgfunction();
-  // }, []);
-
-  // useEffect(() => {
-  //   const imgfunction = async () => {
-  //     const result = await axios.get("https://dog.ceo/api/breeds/image/random");
-  //     setLoadImg4(result.data.message);
-  //     console.log(result);
-  //   };
-  //   imgfunction();
-  // }, []);
-
-  // useEffect(() => {
-  //   const imgfunction = async () => {
-  //     const result = await axios.get("https://dog.ceo/api/breeds/image/random");
-  //     setLoadImg5(result.data.message);
-  //     console.log(result);
-  //   };
-  //   imgfunction();
-  // }, []);
-
-  // useEffect(() => {
-  //   const imgfunction = async () => {
-  //     const result = await axios.get("https://dog.ceo/api/breeds/image/random");
-  //     setLoadImg6(result.data.message);
-  //     console.log(result);
-  //   };
-  //   imgfunction();
-  // }, []);
-
+  const onClickVelog = () => {
+    window.open("https://velog.io/@aimzero9303");
+  };
   return (
     <Wrapper>
-      {/* <Photo1>
-          <Photo1Img src={loadImg} />
-      </Photo1>
-      <Photo2>
-        <Photo2Img src={loadImg2} />
-        <Photo2Img src={loadImg3} />
-        <Photo2Img src={loadImg4} />
-      </Photo2>
-      <Photo3>
-        <Photo3Img src={loadImg5} />
-        <Photo3Img src={loadImg6} />
-      </Photo3> */}
       <Box1>
         <Box1_Zoom>
           <Box1_Zoom_Wrapper>
-            <Box1_Title style={{ color: "white" }}>안녕하세요</Box1_Title>
-            <Box1_Content style={{ color: "white" }}>
+            <HelloLottie src="https://embed.lottiefiles.com/animation/76787"></HelloLottie>
+            {/* <Box1_Title>안녕하세요!</Box1_Title> */}
+            <Box1_Content>
                사용자의 입장에서 코드를 작성하는 프론트엔드 개발자 조준영
-              입니다!
+              입니다.
             </Box1_Content>
+            <Box1_Btns>
+              <Tooltip placement="bottom" title={"Visit GitHub"}>
+                <Box1_Btn_Github
+                  src="https://cdn-icons-png.flaticon.com/512/25/25231.png"
+                  onClick={onClickGithub}
+                />
+              </Tooltip>
+              <Tooltip placement="bottom" title={"Visit Velog"}>
+                <Box1_Btn_Velog
+                  src="https://cdn-icons.flaticon.com/png/512/3669/premium/3669981.png?token=exp=1657452407~hmac=71b8479dbe68314d5d35e977743f13ab"
+                  onClick={onClickVelog}
+                />
+              </Tooltip>
+            </Box1_Btns>
           </Box1_Zoom_Wrapper>
         </Box1_Zoom>
       </Box1>
+      <Box2>
+        <Box2_Slide bottom>
+          <Box2_Content_Wrapper>
+            <Box2_Content>
+              React, Next.js, Recoil, Emotion, Apollo-Client, Graphql을 사용한
+              웹 개발 경험이 있습니다.
+            </Box2_Content>
+            <Box2_Content>
+              비록 신입이지만 1-2년 경력의 개발자 역량을 갖췄다고 생각합니다.
+            </Box2_Content>
+          </Box2_Content_Wrapper>
+        </Box2_Slide>
+        <ReactLottie src="https://embed.lottiefiles.com/animation/93699"></ReactLottie>
+      </Box2>
+      <Box3>
+        <ThinkLottie src="https://embed.lottiefiles.com/animation/89660"></ThinkLottie>
+        <Box3_Slide>
+          <Box3_Content_Wrapper>
+            <Box3_Content>
+              평상시 웹서비스를 이용하면서 개인이 콘텐츠를 생산해 양방향으로
+              소통하는 페이스북의 등장과, 소셜로그인과 같이 기존의 로그인 보다
+              한층 더 편해진 서비스들을 접하면서, 이 분야의 전망과 관심이
+              생겼고, 첫 코딩으로 국비지원 자바스프링 풀스텍을 배웠습니다.
+            </Box3_Content>
+            <Box3_Content>
+              처음으로 웹개발을 접하면서, 내가 어떻게 코드를 작성하는지에 따라
+              사용자에게 직접적인 경험을 주는 프론트엔드가 저와 맞는다고 생각이
+              들었고 확신이 섰습니다.
+            </Box3_Content>
+          </Box3_Content_Wrapper>
+        </Box3_Slide>
+      </Box3>
+      <Box4>
+        <Box4_Slide>
+          <Box4_Content_Wrapper>
+            <Box4_Content>
+              졸업 후, 프론트엔드 개발자가 되어야겠다고 마음을 먹은 후, 리액트
+              훅스 등 최신 기술 스택을 이용한 12주 과정 프론트엔드 부트캠프
+              코드캠프를 수료했습니다.
+            </Box4_Content>
+            <Box4_Content>(2022. 03. 14 ~ 2022. 06. 03)</Box4_Content>
+          </Box4_Content_Wrapper>
+        </Box4_Slide>
+        <StudyLottie src="https://embed.lottiefiles.com/animation/90714"></StudyLottie>
+      </Box4>
+      <Box5>
+        <Box5_Content_Wrapper>
+          <Box5_Content>
+            부트캠프 중 매주 랜덤으로 페어가 바뀌었고, 한 주의 평가를 페어에게
+            받았습니다.
+          </Box5_Content>
+          <Box5_Content>
+            리뷰에서 코드가 창의적이라는 평가와 커뮤니케이션에서 긍정적인 평가는
+            빠진적이 없습니다.
+          </Box5_Content>
+        </Box5_Content_Wrapper>
+        <Box5_Review_Wrapper>
+          <Box5_Review></Box5_Review>
+          <Box5_Review></Box5_Review>
+          <Box5_Review></Box5_Review>
+        </Box5_Review_Wrapper>
+      </Box5>
     </Wrapper>
   );
 }
--- freeboard_frontend/src/components/units/Home.styles.ts
+++ freeboard_frontend/src/components/units/Home.styles.ts
@@ -1,34 +1,178 @@
 import styled from "@emotion/styled";
 import Zoom from "react-reveal/Zoom";
+import Flip from "react-reveal/Flip";
+import Slide from "react-reveal/Slide";
 
 export const Wrapper = styled.div`
   width: 100%;
-  height: 100vh;
   display: flex;
   flex-direction: column;
   margin-top: -465px;
 `;
 
-export const Box1 = styled.div`
-  background-image: url("https://gobooki.net/wp-content/uploads/2021/07/what-is-coding-1.jpg");
+export const Box1 = styled.section`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  background: url("/images/rending/rending1.jpg") fixed;
   background-size: cover;
   width: 100%;
-  height: 500px;
+  height: 100vh;
+`;
+
+export const HelloLottie = styled.iframe`
+  border: none;
+`;
+
+export const Box1_Btns = styled.div`
+  margin-top: 20px;
+  display: flex;
+  justify-content: space-around;
+`;
+
+export const Box1_Btn_Github = styled.img`
+  margin-right: 50px;
+  width: 35px;
+  height: 35px;
+  cursor: pointer;
+`;
+
+export const Box1_Btn_Velog = styled.img`
+  width: 35px;
+  height: 35px;
+  cursor: pointer;
 `;
 
 export const Box1_Zoom = styled(Zoom)``;
 
 export const Box1_Zoom_Wrapper = styled.div`
+  width: 100%;
   display: flex;
   flex-direction: column;
   align-items: center;
 `;
 
 export const Box1_Title = styled.h1`
-  margin-top: 150px;
+  margin-top: 90px;
   font-size: 50px;
+  font-weight: 300;
+  color: black;
 `;
 
 export const Box1_Content = styled.p`
   font-size: 25px;
+  font-weight: 300;
+  color: black;
+`;
+
+export const Box2 = styled.section`
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  height: 400px;
+  padding-top: 80px;
+`;
+
+export const Box2_Content_Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding-top: 100px;
+`;
+
+export const Box2_Slide = styled(Slide)``;
+
+export const ReactLottie = styled.iframe`
+  border: none;
+  margin-left: 150px;
+`;
+
+export const Box2_Content = styled.p`
+  font-size: 23px;
+  font-weight: 200;
+  width: 600px;
+`;
+
+export const Box3 = styled.section`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  height: 400px;
+`;
+
+export const Box3_Content_Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-top: 75px;
+`;
+
+export const Box3_Slide = styled(Slide)``;
+
+export const Box3_Content = styled.p`
+  font-size: 22px;
+  font-weight: 200;
+  width: 610px;
+`;
+
+export const ThinkLottie = styled.iframe`
+  border: none;
+  margin-right: 150px;
+`;
+
+export const Box4 = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  height: 400px;
+`;
+
+export const Box4_Slide = styled(Slide)``;
+
+export const Box4_Content = styled.p`
+  font-size: 23px;
+  font-weight: 200;
+  width: 550px;
+`;
+
+export const Box4_Content_Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-top: 120px;
+`;
+
+export const StudyLottie = styled.iframe`
+  border: none;
+  margin-left: 180px;
+`;
+
+export const Box5 = styled.section`
+  background-color: #f4f4f4;
+  height: 1000px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const Box5_Content_Wrapper = styled.div`
+  margin-bottom: 50px;
+`;
+
+export const Box5_Content = styled.p`
+  font-size: 25px;
+  font-weight: 200;
+`;
+
+export const Box5_Review_Wrapper = styled.div`
+  display: flex;
+`;
+
+export const Box5_Review = styled.div`
+  :last-child {
+    margin-right: 0px;
+  }
+  margin-right: 40px;
+  width: 300px;
+  height: 530px;
+  background-color: white;
+  border-radius: 15px;
 `;
